d5b71f78a748b55f5a8a85e28a55c9c181a63995
George <gnthackray@btinternet.com>
2014-01-02 00:01:30 +0000

    deletions

--- Genealogy/TancWebApp/scripts/ForceDirect/map_edge.js
+++
@@ -1,8 +0,0 @@
-﻿
-var Edge = function (id, source, target, data) {
-    this.id = id;
-    this.source = source;
-    this.target = target;
-    this.data = typeof (data) !== 'undefined' ? data : {};
-};
-
