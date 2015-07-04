cd9c39b1f0529e1258b390fb72f18c750255e884
George N Thackray <gnthackray@btinternete.com>
2015-07-04 01:55:57 +0100

    centralising paths

---
+++ Genealogy/TancWebApp/Client/scripts/libs/debounce/debounce.js
@@ -0,0 +1,45 @@
+﻿
+//USAGE EXAMPLES 
+
+// using debounce in a constructor or initialization function to debounce
+// focus TotalEvents for a widget (onFocus is the original handler):
+//this.debouncedOnFocus = this.onFocus.debounce(500, false);
+//this.inputNode.addEventListener('focus', this.debouncedOnFocus, false);
+
+//// to coordinate the debounce of a method for all objects of a certain class, do this:
+//MyClass.prototype.someMethod = function () {
+//    /* do something here, but only once */
+//} .debounce(100, true); // execute at start and use a 100 msec detection period
+
+//// wait until the user is done moving the mouse, then execute
+//// (using the stand-alone version)
+//document.onmousemove = debounce(function (e) {
+//    /* do something here, but only once after mouse cursor stops */
+//}, 250, false);
+//http://ajaxian.com/archives/debounce-your-javascript-functions
+Function.prototype.debounce = function (threshold, execAsap) {
+    var func = this, // reference to original function
+            timeout; // handle to setTimeout async task (detection period)
+    // return the new debounced function which executes the original function only once
+    // until the detection period expires
+    return function debounced() {
+        var obj = this, // reference to original context object
+                args = arguments; // arguments at execution time
+        // this is the detection function. it will be executed if/when the threshold expires
+        function delayed() {
+            // if we're executing at the end of the detection period
+            if (!execAsap)
+                func.apply(obj, args); // execute now
+            // clear timeout handle
+            timeout = null;
+        };
+        // stop any current detection period
+        if (timeout)
+            clearTimeout(timeout);
+            // otherwise, if we're not already waiting and we're executing at the beginning of the waiting period
+        else if (execAsap)
+            func.apply(obj, args); // execute now
+        // reset the waiting period
+        timeout = setTimeout(delayed, threshold || 100);
+    };
+}
---
+++ Genealogy/TancWebApp/Client/scripts/libs/selectionTool/selectorParishs.js
@@ -0,0 +1,83 @@
+﻿
+
+
+var AncSelectorParishs = function () {
+    this.qryStrUtils = new QryStrUtils();
+    this.ancUtils = new AncUtils();
+    this.ancSelectorBase = new AncSelectorBase()
+
+    this.url = "/ParishService/GetParishs/Select"; // getHost() + 
+
+    this.ancSelectorBase.context_data = {
+        editorUrl: '../HtmlPages/ParishEditor.html',
+        param_name: 'pids',
+        selectionUrl: '/ParishService/GetParishNames',
+        pagerfunction: this.getParishLink,   // this.pagerfunction = 'getLink';
+        search_body: 'p_search_bdy',
+        search_hed: 'p_search_hed',
+        pager: 'p_pager',
+        sourceRefId: 'txtParishRef',
+        title: 'PARISHS',
+        refreshMethod: this.init,
+        parentContext: this,
+        name: 'sp'
+    };
+
+}
+
+
+AncSelectorParishs.prototype = {
+
+    init: function(selectorid) {
+
+        var params = {};
+
+        if (selectorid != undefined) {
+            this.ancSelectorBase.createOutline(selectorid)
+        }
+
+        var parishName = $('#' + this.ancSelectorBase.context_data.sourceRefId).val();
+
+        if (parishName == '') parishName = '%';
+
+        params[0] = '%';//deposited
+        params[1] = parishName;
+        params[2] = '%';//county
+        params[3] = this.qryStrUtils.getParameterByName('p_page', 0);
+        params[4] = '10';
+        params[5] = 'ParishName'; //sort column
+
+        var success = function (data) {
+            var batch_data = {};
+
+            batch_data.Batch = data.Batch;
+            batch_data.BatchLength = data.BatchLength;
+            batch_data.Total = data.Total;
+            batch_data.rows = new Array();
+
+            $.each(data.serviceParishs, function (source, sourceInfo) {
+                var row0 = { id: sourceInfo.ParishId, ref: sourceInfo.ParishName };
+                batch_data.rows.push(row0);
+            });
+
+            this.ancSelectorBase.createBody(batch_data);
+        };
+
+        this.ancUtils.twaGetJSON(this.url, params, $.proxy(success, this));
+
+        return false;
+    },
+
+    getParishLink: function(toPage) {
+
+        this.qryStrUtils.updateQryPar('p_page', toPage);
+        this.init();
+
+    }
+
+
+
+}
+
+
+
