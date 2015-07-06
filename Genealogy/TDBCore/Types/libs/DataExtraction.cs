d66d0975ec2195360f640098ebfb7ea1b7fa3dfb
George N Thackray <gnthackray@btinternete.com>
2015-07-06 23:21:48 +0100

    moving things around

--- Genealogy/TDBCore/Types/libs/DataExtraction.cs
+++ Genealogy/TDBCore/Types/libs/DataExtraction.cs
@@ -1,14 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Net;
-using System.Text;
-using System.Text.RegularExpressions;
-using GedcomParser;
-using TDBCore.BLL;
 
 namespace TDBCore.Types.libs
 {
