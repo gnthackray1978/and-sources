7859d2cb4fcc658125941845b18c48a1a3df1b9c
George N Thackray <gnthackray@btinternete.com>
2015-07-13 23:57:24 +0100

    adding log table wip

---
+++ Genealogy/TDBCore/Types/DTOs/LogDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TDBCore.Types.DTOs
+{
+    public class LogDto
+    {
+        public int LogId { get; set; }
+        public DateTime LogDate { get; set; }
+        public int LogType { get; set; }
+
+        public string LogData { get; set; }
+
+    }
+}
---
+++ Genealogy/TDBCore/Types/DTOs/LogSearch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TDBCore.Types.DTOs
+{
+    public class LogSearch
+    {
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+
+        public int LogType { get; set; }
+    }
+}
