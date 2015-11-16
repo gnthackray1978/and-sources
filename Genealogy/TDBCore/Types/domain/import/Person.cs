d1abd81ce4a6641ad3e12fa3608912bf095ee160
George <gnthackray@btinternet.com>
2015-11-16 10:38:57 +0000

    changes

---
+++ Genealogy/TDBCore/Types/domain/import/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDBCore.Types.domain.import
+{
+    public class ImpPerson
+    {
+        //SubjectChristianName	SubjectSurname	SubjectOccupation	SubjectRelation	OthersideChristianName	OthersideSurname	OthersideOccupation
+
+        public string ChristianName { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Occupation { get; set; }
+
+        public string Relation { get; set; }
+
+        public string OthersideChristianName { get; set; }
+
+        public string OthersideSurname { get; set; }
+
+        public string OthersideOccupation { get; set; }
+
+        public string ToCSVString(bool includeTrailingComma)
+        {
+            return ChristianName + "," + Surname + "," + Occupation + "," + Relation + "," + OthersideChristianName +
+                   "," + OthersideSurname + "," + OthersideOccupation + (includeTrailingComma ? "," : "");
+
+        }
+    }
+}
