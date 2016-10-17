8dd5500590ca97e32c1d660c671d4afba69e3e52
George <gnthackray@btinternet.com>
2016-10-17 02:08:09 +0100

    working on new api

--- Genealogy/TDBCore/Types/validators/ParishValidator.cs
+++ Genealogy/TDBCore/Types/validators/ParishValidator.cs
@@ -25,19 +25,43 @@ namespace TDBCore.Types.validators
         //500
         public bool ValidName()
         {
-            return ServiceParish.ParishName.Length >= 3 && ServiceParish.ParishName.Length <= 500;
+            if (ServiceParish.ParishName.Length >= 3 && ServiceParish.ParishName.Length <= 500)
+            {
+                return true;
+            }
+
+            _errorState += Environment.NewLine + "Invalid ParishName Value";
+
+            return false;
         }
 
         //50
         public bool ValidCounty()
         {
-            return ServiceParish.ParishCounty.Length >= 3 && ServiceParish.ParishCounty.Length <= 50;
+
+            if (ServiceParish.ParishCounty.Length >= 3 && ServiceParish.ParishCounty.Length <= 50)
+            {
+                return true;
+            }
+
+            _errorState += Environment.NewLine + "Invalid ParishCounty Value";
+
+            return false;
+
         }
         //100
         public bool ValidDeposited()
         {
-            return ServiceParish.ParishDeposited.Length >= 3 && ServiceParish.ParishDeposited.Length <= 100;
-                          
+           
+            if (ServiceParish.ParishDeposited.Length >= 3 && ServiceParish.ParishDeposited.Length <= 100)
+            {
+                return true;
+            }
+
+
+            _errorState += Environment.NewLine + "Invalid ParishDeposited Value";
+
+            return false;
         }
 
     }
